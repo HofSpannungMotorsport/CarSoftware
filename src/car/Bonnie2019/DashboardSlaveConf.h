bc78d5559e8b1de18d4ef5af0a33316fb2619d72
Flo B <thebraunber@googlemail.com>
2018-09-17 09:00:34 +0000

    Extended config of Dashboard-slave for bonnie

--- src/car/Bonnie2019/DashboardSlaveConf.h
+++ src/car/Bonnie2019/DashboardSlaveConf.h
@@ -3,10 +3,14 @@
 
 #include "HardwareLed.h"
 #include "mbed.h"
+#include "../../can/can_config.h"
+
+CAN can(CAN1_CONF)
 
 HardwareLed ledGreen(D6);
 HardwareLed ledYellow(D5);
 HardwareLed ledRed(D3);
+
 
 // Buttons
 //BTN-Oins: D9, D7
