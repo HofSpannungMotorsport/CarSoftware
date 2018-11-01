66672c2e503f38e50b5244d85bc17ee6c71c8a48
braunyyy <thebraunber@googlemail.com>
2018-11-01 18:41:22 +0100

    Changed button-states to hexadecimal represantation

--- src/components/interface/IButton.h
+++ src/components/interface/IButton.h
@@ -6,10 +6,10 @@
 typedef uint8_t button_status_t;
 
 enum button_error_type_t : uint8_t {
-    BUTTON_ERROR =                  1,
-    STATE_BUFFER_FULL =             2,
-    WRONG_STATE =                   4,
-    ADDED_SAME_STATE_AS_LAST_TIME = 8
+    BUTTON_ERROR =                  0x1,
+    STATE_BUFFER_FULL =             0x2,
+    WRONG_STATE =                   0x4,
+    ADDED_SAME_STATE_AS_LAST_TIME = 0x8
 };
 
 enum button_state_t : uint8_t {
