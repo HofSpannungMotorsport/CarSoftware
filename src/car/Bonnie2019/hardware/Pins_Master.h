a407c32ee29c1bf75262cfcff0b73c19cc782b20
Timo <timoxd7@gmx.de>
2018-11-19 18:55:40 +0100

    Update Brake Service (FSG Rules), Update Pins

--- src/car/Bonnie2019/hardware/Pins_Master.h
+++ src/car/Bonnie2019/hardware/Pins_Master.h
@@ -1,11 +1,11 @@
 #ifndef PINS_MASTER_H
 #define PINS_MASTER_H
 
-#define MASTER_PIN_BRAKE_LIGHT              D9
-#define MASTER_PIN_BUZZER                   D7
-#define MASTER_PIN_FAN                      D10
-#define MASTER_PIN_PUMP_PWM                 D6
-#define MASTER_PIN_SPRING_TRAVEL_SENSOR_HL  A0
-#define MASTER_PIN_SPRING_TRAVEL_SENSOR_HR  A1
+#define MASTER_PIN_BRAKE_LIGHT              PG_4
+#define MASTER_PIN_BUZZER                   PE_13
+#define MASTER_PIN_FAN                      PD_10
+#define MASTER_PIN_PUMP_PWM                 PE_10
+#define MASTER_PIN_SPRING_TRAVEL_SENSOR_HL  PF_4
+#define MASTER_PIN_SPRING_TRAVEL_SENSOR_HR  PB_1
 
 #endif // PINS_MASTER_H
--- src/components/service/BrakeLightService.h
+++ src/components/service/BrakeLightService.h
@@ -12,29 +12,26 @@
 class BrakeLightService : public IService {
     public:
         BrakeLightService(CarService &carService, IPedal* brakePedal, ILed* brakeLight)
-            : _carService(carService), _brakePedal(brakePedal), _brakeLight(brakeLight) {}
+            : _carService(carService), _brakePedal(brakePedal), _brakeLight(brakeLight) {
+            _brakeLight->setBlinking(BLINKING_OFF);
+            _brakeLight->setBrightness(1);
+        }
 
         virtual void run() {
+            // If Brakeing -> light on
+            // If not Brakeing -> light off
+            // If Error or something else -> light on
             if (_carService.getState() == READY_TO_DRIVE) {
                 if (_brakePedal->getValue() >= STD_BRAKE_LIGHT_THRESHHOLD) {
-                    _brakeLight->setBrightness(1);
                     _brakeLight->setState(LED_ON);
-
-                    if (_brakePedal->getValue() >= STD_HARD_BRAKE_LIGHT_FLASH_THRESHHOLD) {
-                        _brakeLight->setBlinking(BLINKING_FAST);
-                    } else {
-                        _brakeLight->setBlinking(BLINKING_OFF);
-                    }
                 } else {
                     _brakeLight->setState(LED_OFF);
                 }
             } else {
-                _brakeLight->setBrightness(1);
                 _brakeLight->setState(LED_ON);
-                _brakeLight->setBlinking(BLINKING_NORMAL);
             }
         }
-    
+
     protected:
         CarService &_carService;
         IPedal* _brakePedal;
