a98805e5c221a3f72caf1e10d09808dc3f7ebd70
Timo <timoxd7@gmx.de>
2019-03-18 22:17:04 +0100

    Update UnitTest.h

--- hardwareTest/UnitTest.h
+++ hardwareTest/UnitTest.h
@@ -2,13 +2,14 @@
 // runtime.setup() gets called at the beginning,
 // runtime.loop() gets called then over and over again...
 
-// Include the test program here
-#include ".cpp"
-
 #ifndef MESSAGE_REPORT
     #define MESSAGE_REPORT
     Serial pcSerial(USBTX, USBRX); // Connection to PC over Serial
 #endif
+
+
+// Include the test program here
+#include ".cpp"
 
 class UnitTest {
     public:
