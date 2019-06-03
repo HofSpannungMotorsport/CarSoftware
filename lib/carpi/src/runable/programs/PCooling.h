9261909a159186a92be698bb5ecfb68a199a59f1
Timo <timoxd7@gmx.de>
2019-06-03 09:59:35 +0200

    Update Cooling to match new BamocarD3 Version

--- lib/carpi/src/runable/programs/PCooling.h
+++ lib/carpi/src/runable/programs/PCooling.h
@@ -14,17 +14,17 @@
 #define STD_COOLING_FAN_ON_UNTIL_SPEED 5 // kM/h
 
 // Temperatures at which the Pump will start or reach its maximum
-#define STD_MOTOR_TEMP_START_PUMP   40 // °C
+#define STD_MOTOR_TEMP_START_PUMP   30 // °C
 #define STD_MOTOR_TEMP_FULL_PUMP    60 // °C
-#define STD_CONTROLLER_TEMP_START_PUMP  35 // °C
-#define STD_CONTROLLER_TEMP_FULL_PUMP    40 // °C
+#define STD_CONTROLLER_TEMP_START_PUMP  30 // °C
+#define STD_CONTROLLER_TEMP_FULL_PUMP   40 // °C
 
 // Absolut Max temperatures which will stop the car to prevent overheating
 #define STD_MAX_MOTOR_TEMP 110 // °C (120 in datasheet, but we don't want to stress the limit)
 #define STD_MAX_CONTROLLER_TEMP 65 // °C
 
 enum cooling_service_error_type_t {
-    COOLING_SERVICE_ERROR_UNDEFINED = 0x0,
+    COOLING_SERVICE_OK = 0x0,
     COOLING_SERVICE_MOTOR_OVERHEAT = 0x1,
     COOLING_SERVICE_CONTROLLER_OVERHEAT = 0x2
 };
@@ -61,12 +61,12 @@ class PCooling : public IProgram {
                     _fan->setState(FAN_OFF);
                 }
 
-                int16_t motorTemp = 60;//_motorController->getMotorTemp();
-                int16_t controllerTemp = 40;//_motorController->getServoTemp();
+                float motorTemp = _motorController->getMotorTemp();
+                float controllerTemp = _motorController->getServoTemp();
 
                 // Set Pump speed according to the temperature of the devices
-                float pumpMotor = ((float)motorTemp - (float)STD_MOTOR_TEMP_START_PUMP) * 1.0 / ((float)STD_MOTOR_TEMP_FULL_PUMP - (float)STD_MOTOR_TEMP_START_PUMP);
-                float pumpController = ((float)controllerTemp - (float)STD_CONTROLLER_TEMP_START_PUMP) * 1.0 / ((float)STD_CONTROLLER_TEMP_FULL_PUMP - (float)STD_CONTROLLER_TEMP_START_PUMP);
+                float pumpMotor = ((float)motorTemp - (float)STD_MOTOR_TEMP_START_PUMP) / ((float)STD_MOTOR_TEMP_FULL_PUMP - (float)STD_MOTOR_TEMP_START_PUMP);
+                float pumpController = ((float)controllerTemp - (float)STD_CONTROLLER_TEMP_START_PUMP) / ((float)STD_CONTROLLER_TEMP_FULL_PUMP - (float)STD_CONTROLLER_TEMP_START_PUMP);
 
                 // Use the highest power given from the "hottest" device
                 float newPumpPower = 1;
@@ -83,9 +83,7 @@ class PCooling : public IProgram {
                     newPumpPower = 0;
 
                 // Apply pump power
-                //_pump->setSpeed(newPumpPower);
-                // [QF]
-                _pump->setSpeed(1);
+                _pump->setSpeed(newPumpPower);
 
                 // At least after setting the pump, check if the temperature of any device is too high
                 if (motorTemp > STD_MAX_MOTOR_TEMP) {
