cb50bb2ef83d19e1f612ac0b82a3ab7e0f046757
Timo <timoxd7@gmx.de>
2019-06-12 10:09:13 +0200

    Bugfix

--- car/Bonnie2019/hardware/Pins_Master_PCB.h
+++ car/Bonnie2019/hardware/Pins_Master_PCB.h
@@ -26,7 +26,7 @@
 
 // HV Enable
 #define MASTER_PIN_HV_ENABLED_TSMS          PG_0
-#define MASTER_PIN_HV_ENABLED_AIR+          PE_4
+#define MASTER_PIN_HV_ENABLED_AIR           PE_4
 
 // CAN Car intern
 #define MASTER_PIN_CAR_INTERN_CAN_RD        PD_0
