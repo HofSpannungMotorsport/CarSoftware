a7e1ef2636139cf621217e9b5c8c2ad2b38eff47
Timo <timoxd7@gmx.de>
2019-10-11 01:39:27 +0200

    Update Adapter Pins

--- car/Bonnie2019/hardware/Pins_Master_PCB_Adapter.h
+++ car/Bonnie2019/hardware/Pins_Master_PCB_Adapter.h
@@ -10,7 +10,7 @@
 
 // Dout Enable Pins for Motor Controller
 #define MASTER_PIN_RFE_ENABLE               PD_2
-#define MASTER_PIN_RUN_ENABLE               PC_14
+#define MASTER_PIN_RUN_ENABLE               PC_3
 
 // Dout Brake Light
 #define MASTER_PIN_BRAKE_LIGHT              PB_7
@@ -22,7 +22,7 @@
 #define MASTER_PIN_PUMP_PWM                 PA_0
 
 // Dout Cooling Fans
-#define MASTER_PIN_FAN                      PC_15
+#define MASTER_PIN_FAN                      PA_6
 
 // Din HV Enable
 #define MASTER_PIN_TSMS                     PH_1
@@ -30,12 +30,12 @@
 #define MASTER_PIN_60V_OK                   PA_15
 
 // CAN Car intern
-#define MASTER_PIN_CAR_INTERN_CAN_RD        PB_8
-#define MASTER_PIN_CAR_INTERN_CAN_TD        PB_9
+#define MASTER_PIN_CAR_INTERN_CAN_RD        PB_5
+#define MASTER_PIN_CAR_INTERN_CAN_TD        PB_6
 
 // CAN Motor Controller
-#define MASTER_PIN_MOTOR_CONTROLLER_CAN_RD  PB_5
-#define MASTER_PIN_MOTOR_CONTROLLER_CAN_TD  PB_6
+#define MASTER_PIN_MOTOR_CONTROLLER_CAN_RD  PB_8
+#define MASTER_PIN_MOTOR_CONTROLLER_CAN_TD  PB_9
 
 // Dout LED HV Enabled
 #define MASTER_PIN_LED_HV_ENABLED           PC_10 // Not Connected
