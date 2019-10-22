876123b0fa8001aba64b7e0bf22cce0b6bc732e9
Timo <timoxd7@gmx.de>
2019-10-22 18:37:34 +0200

    Update Travis

--- .ci/setup-ssh.sh
+++ .ci/setup-ssh.sh
@@ -4,14 +4,6 @@
 set -ev
 declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/travis_temp_ssh_key_XXXX)"
 # Decrypt the file containing the private key (put the real name of the variables)
-openssl aes-256-cbc \
-  -K $encrypted_b6597a4a9f0d_key \
-  -iv $encrypted_b6597a4a9f0d_iv \
-  -in ".ci/.ssh/ssh-key.enc" \
-  -out "$SSH_FILE" -d
+openssl aes-256-cbc -K $encrypted_b6597a4a9f0d_key -iv $encrypted_b6597a4a9f0d_iv -in ".ci/.ssh/ssh-key.enc" -out "$SSH_FILE" -d
 # Enable SSH authentication
-chmod 600 "$SSH_FILE" \
-  && printf "%s\n" \
-       "Host github.com" \
-       "  IdentityFile $SSH_FILE" \
-       "  LogLevel ERROR" >> ~/.ssh/config
+chmod 600 "$SSH_FILE" && printf "%s\n" "Host github.com" "  IdentityFile $SSH_FILE" "  LogLevel ERROR" >> ~/.ssh/config
