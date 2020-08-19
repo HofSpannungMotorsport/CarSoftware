f2392c27716fcffc75153868ec3bec198ac02a06
Patrick Bär <pbaer@dc-solution.de>
2020-08-19 12:22:48 +0200

    improved Lora.h

--- lib/carpi/lib/LoRa/LoRa.h
+++ lib/carpi/lib/LoRa/LoRa.h
@@ -7,8 +7,7 @@
 
 #ifdef FEATURE_LORA
 
-/* Set this flag to '1' to display debug messages on the console */
-#define DEBUG_MESSAGE   1
+#define DEBUG_MESSAGE
 
 #define RF_FREQUENCY            RF_FREQUENCY_868_1  // Hz
 #define TX_OUTPUT_POWER         14                  // 14 dBm
@@ -26,12 +25,9 @@
 #define LORA_CRC_ENABLED        true
 
 
-#define RX_TIMEOUT_VALUE    3500	// in ms
+#define RX_TIMEOUT_VALUE        3500	// in ms
 
-//#define BUFFER_SIZE       32        // Define the payload size here
-#define BUFFER_SIZE         64        // Define the payload size here
-
-Serial pcSerial(USBTX, USBRX);
+#define LORA_BUFFER_SIZE        64        // Define the payload size here
 
 
 /*
@@ -53,20 +49,10 @@ typedef enum
     CAD_DONE
 } AppStates_t;
 
-volatile AppStates_t State = LOWPOWER;
-
 /*!
 * Radio events function pointer
 */
 static RadioEvents_t RadioEvents;
-
-/*
-*  Global variables declarations
-*/
-SX1276Generic *Radio;
-
-
-DigitalOut *led3;
 
 void OnTxDone(void *radio, void *userThisPtr, void *userData);
 void OnRxDone(void *radio, void *userThisPtr, void *userData, uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr);
@@ -77,32 +63,23 @@ void OnRxError(void *radio, void *userThisPtr, void *userData);
 
 class LoRa: public IChannel {
 private:
+    volatile AppStates_t State = LOWPOWER;
+
     Sync &_syncer;
 
-    uint8_t Message[]; //TODO: fill with carMessage
+    uint16_t BufferSize = LORA_BUFFER_SIZE;
+    uint8_t Buffer[LORA_BUFFER_SIZE];
 
-    uint16_t BufferSize = BUFFER_SIZE;
-    uint8_t *Buffer;
+    DigitalOut ledRX;
+    DigitalOut ledTX;
+
+    SX1276Generic Radio;
 
 public:
-    LoRa(Sync &syncer) : _syncer(syncer) {}
-
-    void send(CarMessage &carMessage)()
-    {
-        //TODO: fill with parsed carMessage
-        Message = carMessage;
-
-        DigitalOut *led = new DigitalOut(LED4);   // RX red
-        led3 = new DigitalOut(LED3);  // TX blue
-        
-        Buffer = new uint8_t[BUFFER_SIZE];
-        *led3 = 1;
-
-        Radio = new SX1276Generic(NULL, RFM95_SX1276,
+    LoRa(Sync &syncer) : _syncer(syncer), ledRX(LED4), ledTX(LED3),
+                Radio(NULL, RFM95_SX1276,
                 LORA_SPI_MOSI, LORA_SPI_MISO, LORA_SPI_SCLK, LORA_CS, LORA_RESET,
-                LORA_DIO0, LORA_DIO1, LORA_DIO2, LORA_DIO3, LORA_DIO4, LORA_DIO5);
-        
-        uint8_t i;
+                LORA_DIO0, LORA_DIO1, LORA_DIO2, LORA_DIO3, LORA_DIO4, LORA_DIO5) {
 
         // Initialize Radio driver
         RadioEvents.TxDone = OnTxDone;
@@ -110,31 +87,36 @@ public:
         RadioEvents.RxError = OnRxError;
         RadioEvents.TxTimeout = OnTxTimeout;
         RadioEvents.RxTimeout = OnRxTimeout;
-        if (Radio->Init( &RadioEvents ) == false) {
-            if (DEBUG_MESSAGE) {
-                while(1) {
-                    printf("Radio could not be detected!");
-                    wait_us( 1000000 ); // 1s
-                }
-            }
+        while (Radio.Init( &RadioEvents ) == false) {
+            #ifdef DEBUG_MESSAGE
+                printf("Radio could not be detected!");
+                wait_us( 1000000 ); // 1s
+            #endif
         }
 
-        Radio->SetChannel(RF_FREQUENCY);
+        Radio.SetChannel(RF_FREQUENCY);
 
-        Radio->SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+        Radio.SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
                             LORA_SPREADING_FACTOR, LORA_CODINGRATE,
                             LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
                             LORA_CRC_ENABLED, LORA_FHSS_ENABLED, LORA_NB_SYMB_HOP, 
                             LORA_IQ_INVERSION_ON, 2000 );
 
-        Radio->SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+        Radio.SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
                             LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
                             LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON, 0,
                             LORA_CRC_ENABLED, LORA_FHSS_ENABLED, LORA_NB_SYMB_HOP, 
                             LORA_IQ_INVERSION_ON, true );
+    }
+
+    void send(CarMessage &carMessage)()
+    {        
+        ledTX = 1;
+        
+        uint8_t i = 0;
 
 
-        //Radio->Rx( RX_TIMEOUT_VALUE );
+        //Radio.Rx( RX_TIMEOUT_VALUE );
         State = RX;
         
         while( 1 )
@@ -142,32 +124,34 @@ public:
             switch( State )
             {
             case RX:    // reading happend
+            #ifdef DEBUG_MESSAGE
                 printf("RX");
-                *led3 = 0;
+            #endif
+                ledTX = 0;
                 if( BufferSize > 0 )
                 {
-                    *led = !*led;
-                    memcpy(Buffer, Message, sizeof(Message));
+                    ledRX = !ledRX;
+                    memcpy(Buffer, carMessage, sizeof(carMessage));
                     // We fill the buffer with numbers for the payload 
-                    for( i = sizeof(Message); i < BufferSize; i++ )
+                    for( i = sizeof(carMessage); i < BufferSize; i++ )
                     {
-                        Buffer[i] = i - sizeof(Message);
+                        Buffer[i] = i - sizeof(carMessage);
                     }
                     wait_us( 10000 );   // 10ms
-                    Radio->Send( Buffer, BufferSize );
+                    Radio.Send( Buffer, BufferSize );
                 }
                 State = LOWPOWER;
                 break;
             case TX:    // sending happend
-                *led3 = 1;
-                Radio->Rx( RX_TIMEOUT_VALUE );
+                ledTX = 1;
+                //Radio.Rx( RX_TIMEOUT_VALUE );
                 State = LOWPOWER;
                 break;
             case RX_TIMEOUT:
                 State = RX;
                 break;
             case RX_ERROR:  // reading error
-                *led = !*led;
+                ledRX = !ledRX;
                 State = RX;
                 break;
             case TX_TIMEOUT:
@@ -182,52 +166,62 @@ public:
             }    
         }
     }
+
+    bool messageInQueue() {
+        // TODO: still to implement
+        return 0;
+    }
 };
 
 
 void OnTxDone(void *radio, void *userThisPtr, void *userData)
 {
-    Radio->Sleep( );
+    Radio.Sleep( );
     State = TX;
-    if (DEBUG_MESSAGE)
+    #ifdef DEBUG_MESSAGE
         printf("> OnTxDone\n");
+    #endif
 }
 
 void OnRxDone(void *radio, void *userThisPtr, void *userData, uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr)
 {
-    Radio->Sleep( );
+    Radio.Sleep( );
     BufferSize = size;
     memcpy( Buffer, payload, BufferSize );
     State = RX;
-    if (DEBUG_MESSAGE)
+    #ifdef DEBUG_MESSAGE
         printf("> OnRxDone: RssiValue=%d dBm, SnrValue=%d \n", rssi, snr);
+    #endif
 }
 
 void OnTxTimeout(void *radio, void *userThisPtr, void *userData)
 {
-    *led3 = 0;
-    Radio->Sleep( );
+    ledTX = 0;
+    Radio.Sleep( );
     State = TX_TIMEOUT;
-    if(DEBUG_MESSAGE)
+    #ifdef DEBUG_MESSAGE
         printf("> OnTxTimeout\n");
+    #endif
 }
 
 void OnRxTimeout(void *radio, void *userThisPtr, void *userData)
 {
-    *led3 = 0;
-    Radio->Sleep( );
+    ledTX = 0;
+    Radio.Sleep( );
     Buffer[BufferSize-1] = 0;
     State = RX_TIMEOUT;
-    if (DEBUG_MESSAGE)
+    #ifdef DEBUG_MESSAGE
         printf("> OnRxTimeout\n");
+    #endif
 }
 
 void OnRxError(void *radio, void *userThisPtr, void *userData)
 {
-    Radio->Sleep( );
+    Radio.Sleep( );
     State = RX_ERROR;
-    if (DEBUG_MESSAGE)
+    #ifdef DEBUG_MESSAGE
         printf("> OnRxError\n");
+    #endif
 }
 
 #endif
